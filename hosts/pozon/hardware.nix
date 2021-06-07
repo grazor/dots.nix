8e095ba51133e96668262a966d4ecbbd502c91c1
Sergey Poryvaev <porivaevs@gmail.com>
2021-06-07 06:09:08 +0300

    Fix pozon hardware config

--- hosts/pozon/hardware.nix
+++ hosts/pozon/hardware.nix
@@ -1,8 +1,6 @@
 { lib, ... }:
 
 {
-  imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
-
   boot.initrd.availableKernelModules =
     [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
