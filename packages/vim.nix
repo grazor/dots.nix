ad18473c4f83b3623fc93354a991d03f1724f484
Sergey Poryvaev <porivaevs@gmail.com>
2021-06-07 06:17:43 +0300

    Add nvim overlay

--- packages/vim.nix
+++ packages/vim.nix
@@ -1,13 +1,6 @@
 { pkgs, ... }:
 
 {
-  nixpkgs.overlays = [
-    (import (builtins.fetchTarball {
-      url =
-        "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
-    }))
-  ];
-
   environment.variables = { EDITOR = "nvim"; };
 
   environment.systemPackages = with pkgs; [
