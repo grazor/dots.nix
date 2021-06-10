7b9188c0fac8de34d5b4c498f3897532f6232de8
Sergey Poryvaev <porivaevs@gmail.com>
2021-06-11 01:07:41 +0300

    Use latest kernel

--- hosts/pozon/hardware.nix
+++ hosts/pozon/hardware.nix
@@ -1,8 +1,9 @@
-{ lib, ... }:
+{ pkgs, lib, ... }:
 
 {
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
