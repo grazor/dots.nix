a490c4683cf4e8387c2c924b694ab29a7a010550
Sergey Poryvaev <porivaevs@gmail.com>
2021-06-23 17:05:20 +0300

    Fix java apps

--- home/g/default.nix
+++ home/g/default.nix
@@ -3,7 +3,8 @@
 {
   imports = [ ./sway.nix ./termite.nix ./nvim.nix ./bin.nix ];
 
-  home.packages = with pkgs; [
-    watson # time tracker
-  ];
+  home.packages = with pkgs;
+    [
+      watson # time tracker
+    ];
 }
--- hosts/pozon/arduino.nix
+++ hosts/pozon/arduino.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    arduino
-  ];
+  environment.systemPackages = with pkgs; [ arduino ];
+
+  environment.sessionVariables = { "_JAVA_AWT_WM_NONREPARENTING" = "1"; };
 }
