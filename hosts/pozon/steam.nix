1c1dce2bbb1586318ac78f9f038056a417eecb17
Sergey Poryvaev <porivaevs@gmail.com>
2021-06-24 16:15:03 +0300

    Pipewire libs

--- hosts/pozon/steam.nix
+++ hosts/pozon/steam.nix
@@ -7,12 +7,14 @@
       driSupport = true;
       driSupport32Bit = true;
       extraPackages = with pkgs; [ vaapiVdpau ];
+      extraPackages32 = with pkgs.pkgsi686Linux;
+        [ libva ] ++ lib.optionals config.services.pipewire.enable [ pipewire ];
     };
   };
 
   nixpkgs.config.packageOverrides = pkgs: {
     steam = pkgs.steam.override {
-      extraPkgs = pkgs: [ pkgs.mono pkgs.libgdiplus ];
+      extraPkgs = pkgs: [ pkgs.mono pkgs.libgdiplus pipewire.lib ];
       extraLibraries = pkgs: [ pkgs.pipewire ];
     };
   };
