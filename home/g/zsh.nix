fb3809d931a53bb2d24f392b1cf00f929534e246
Sergey Porivaev <porivaevs@gmail.com>
2021-07-06 17:14:04 +0300

    Zenbook conf

--- home/g/zsh.nix
+++ home/g/zsh.nix
@@ -5,4 +5,16 @@
     enable = true;
     enableZshIntegration = true;
   };
+
+  programs.zsh = {
+    cdpath = "/home/g/Projects";
+    theme = "agnoster";
+
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
+
+    sessionVariables = {
+      NIX_REMOTE = "daemon";
+    };
+  };
 }
