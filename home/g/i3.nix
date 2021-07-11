041c59e23c8ce7a69cb5585f20aa949b96574910
Sergey Porivaev <porivaevs@gmail.com>
2021-07-11 15:54:16 +0300

    Desktop config

--- home/g/i3.nix
+++ home/g/i3.nix
@@ -27,6 +27,16 @@ in {
       modifier = "Mod4";
 
       keybindings = mkOptionDefault {
+        "${modifier}+h" = "focus left";
+        "${modifier}+j" = "focus down";
+        "${modifier}+k" = "focus up";
+        "${modifier}+l" = "focus right";
+
+        "${modifier}+Shift+h" = "move left";
+        "${modifier}+Shift+j" = "move down";
+        "${modifier}+Shift+k" = "move up";
+        "${modifier}+Shift+l" = "move right";
+
         "${modifier}+0" = "workspace number 10";
         "${modifier}+Shift+0" = "move container to workspace number 10";
 
