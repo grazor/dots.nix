ac09eee10f6548ed8c8ac3128baf23f86ea4002a
Sergey Porivaev <porivaevs@gmail.com>
2021-07-12 20:47:00 +0300

    Tmux

--- shells/default.go.nix
+++ shells/default.go.nix
@@ -1,20 +1,14 @@
 with import <nixpkgs> { };
 
 stdenv.mkDerivation rec {
-  name = "ozon";
+  name = "godev";
   env = buildEnv {
     name = name;
     paths = buildInputs;
   };
 
-  buildInputs = [ gcc go_1_15 golangci-lint protobuf kafkacat delve grpcurl mockgen vault ];
   hardeningDisable = [ "fortify" ];
+  buildInputs = [ gcc go golangci-lint protobuf delve grpcurl mockgen ];
 
-  GOPATH = "/home/g/go:/home/g/Ozon:/home/g/Projects";
-  GONOPROXY = "";
-  GONOSUMDB = "*.ozon.ru";
-  GOPRIVATE = "";
-  GOPROXY = "https://athens.s.o3.ru";
-  GOSUMDB = "sum.golang.org";
-  WARDEN_HOST = "warden.platform.stg.s.o3.ru:82";
+  GOPATH = "/home/g/go:/home/g/Projects";
 }
