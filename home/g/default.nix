27434d1b1096369b5e9c2fd1a3c53212e142ba50
Sergey Poryvaev <porivaevs@gmail.com>
2021-09-09 13:42:12 +0300

    Add dependencies

--- home/g/default.nix
+++ home/g/default.nix
@@ -3,5 +3,5 @@
 {
   imports = [ ./sway.nix ./i3.nix ./termite.nix ./nvim.nix ./bin.nix ./zsh.nix ./tmux.nix ];
 
-  home.packages = with pkgs; [ zoxide ];
+  home.packages = with pkgs; [ zoxide direnv obsidian brightnessctl ripgrep zoom-us ];
 }
