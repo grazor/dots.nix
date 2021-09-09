62d7060a0c83936ce06cb14b63f08215c842cc82
Sergey Poryvaev <porivaevs@gmail.com>
2021-09-09 17:21:29 +0300

    Vim config

--- home/g/nvim/lua/modules/completion/config.lua
+++ home/g/nvim/lua/modules/completion/config.lua
@@ -2,6 +2,10 @@ local config = {}
 
 function config.nvim_lsp()
   require('modules.completion.lspconfig')
+  require('lsp_signature').setup {
+      floating_window_above_cur_line = true,
+      hint_enable = false,
+  }
 end
 
 return config
