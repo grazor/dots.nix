d5fe7b57290ece80d2f0b47ebfef395e18a72706
Sergey Porivaev <porivaevs@gmail.com>
2021-10-12 18:18:59 +0300

    Update paths

--- hosts/pdsk/hardware.nix
+++ hosts/pdsk/hardware.nix
@@ -6,31 +6,26 @@
 {
   hardware.enableRedistributableFirmware = lib.mkDefault true;
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ata_piix" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "nvme" "ata_piix" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
 
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/e4a0f58b-24eb-4c53-9674-d43651f5cb7e";
-    fsType = "ext3";
-  };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/d1340ffd-7e67-4d2f-b468-465994af5ca1";
+      fsType = "ext4";
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/3013-E7A0";
-    fsType = "vfat";
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/8aacce4c-2cda-4738-8a6c-efeedbfdc084";
-    fsType = "ext4";
-  };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/C87A-FB49";
+      fsType = "vfat";
+    };
 
   fileSystems."/tmp" = {
     device = "none";
     fsType = "tmpfs";
-    options = [ "defaults" "size=4G" "mode=1777" ];
+    options = [ "defaults" "size=6G" "mode=1777" ];
   };
 
   swapDevices = [ ];
--- hosts/pozon/default.nix
+++
@@ -1,7 +0,0 @@
-{ lib, ... }:
-
-{
-  imports = [ ./hardware.nix ./gui.nix ./psql.nix ./arduino.nix ./steam.nix ];
-
-  nix.maxJobs = lib.mkDefault 8;
-}
