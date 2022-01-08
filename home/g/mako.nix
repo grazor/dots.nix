3e63def3103843c9de15507161f6d781c7d98b84
Sergey Poryvaev <porivaevs@gmail.com>
2022-01-08 17:39:22 +0300

    Terminal

--- home/g/mako.nix
+++ home/g/mako.nix
@@ -10,10 +10,15 @@
   };
 
   systemd.user.services.mako = {
+    Service.WantedBy = "graphical-session.target";
+    Service.After = "graphical-session.target";
+    Service.PartOf = "graphical-session.target";
+
     Service.Type = "dbus";
     Service.BusName = "org.freedesktop.Notifications";
     Service.ExecStart = "${pkgs.mako}/bin/mako";
     Service.ExecReload = "${pkgs.mako}/bin/mako reload";
+    Service.Restart = "on-failure";
     #restartTriggers = [ config.xdg.configFile."mako/config".source ];
   };
 }
