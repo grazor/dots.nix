a8b772fa054177d373df3b513d5eaa3f20e8026f
Sergey Poryvaev <porivaevs@gmail.com>
2022-02-15 14:25:11 +0300

    Shells

--- shells/default.rust.nix
+++ shells/default.rust.nix
@@ -1,13 +1,15 @@
-with import <nixpkgs> { };
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "rustdev";
+    buildInputs = [
+      gcc openssl
+      rust-analyzer
+      #nixpkgs.latest.rustChannels.nightly.rust
+      (nixpkgs.rustChannelOf { date = "2021-08-09"; channel = "nightly"; }).rust
+    ];
+  }
 
-stdenv.mkDerivation rec {
-  name = "rustdev";
-  env = buildEnv {
-    name = name;
-    paths = buildInputs;
-  };
-
-  nativeBuildInputs = with pkgs; [ rustc cargo gcc openssl pkg-config protobuf ];
-  buildInputs = [ rust-analyzer rustfmt ];
-  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-}
