6da47cd3aa354d4edf984d0048d6180b968844f4
Sergey Poryvaev <porivaevs@gmail.com>
2022-04-04 17:17:03 +0300

    Update rust shell

--- shells/default.rust.nix
+++ shells/default.rust.nix
@@ -1,15 +1,42 @@
-let
-  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-in
-  with nixpkgs;
-  stdenv.mkDerivation {
-    name = "rustdev";
-    buildInputs = [
-      gcc openssl
-      rust-analyzer
-      #nixpkgs.latest.rustChannels.nightly.rust
-      (nixpkgs.rustChannelOf { date = "2021-08-09"; channel = "nightly"; }).rust
-    ];
-  }
+{ pkgs ? import <nixpkgs> {} }:
 
+pkgs.mkShell rec {
+    buildInputs = with pkgs; [
+      llvmPackages_latest.llvm
+      llvmPackages_latest.bintools
+      zlib.out
+      openssl
+      rustup
+      xorriso
+      rust-analyzer
+      pkg-config
+      grub2
+      qemu
+      llvmPackages_latest.lld
+      python3
+    ];
+    RUSTC_VERSION = pkgs.lib.readFile ./rust-toolchain;
+    # https://github.com/rust-lang/rust-bindgen#environment-variables
+    LIBCLANG_PATH= pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+    shellHook = ''
+      export PATH=$PATH:~/.cargo/bin
+      export PATH=$PATH:~/.rustup/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+      '';
+    # Add libvmi precompiled library to rustc search path
+    RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
+      pkgs.libvmi
+    ]);
+    # Add libvmi, glibc, clang, glib headers to bindgen search path
+    BINDGEN_EXTRA_CLANG_ARGS = 
+    # Includes with normal include path
+    (builtins.map (a: ''-I"${a}/include"'') [
+      pkgs.libvmi
+      pkgs.glibc.dev 
+    ])
+    # Includes with special directory paths
+    ++ [
+      ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
+      ''-I"${pkgs.glib.dev}/include/glib-2.0"''
+      ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
+    ];
+}
