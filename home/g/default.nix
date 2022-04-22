f0632c453b525342875b86f24f8a731ac703d7ec
Sergey Poryvaev <porivaevs@gmail.com>
2022-04-22 13:51:04 +0300

    Add qol scripts

--- home/g/default.nix
+++ home/g/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  imports = [ ./sway.nix ./i3.nix ./termite.nix ./nvim.nix ./bin.nix ./zsh.nix ./tmux.nix ];
+  imports = [ ./sway.nix ./i3.nix ./termite.nix ./nvim.nix ./bin.nix ./zsh.nix ./lefthook.nix ];
 
   #home.packages = with pkgs; [ ];
 }
