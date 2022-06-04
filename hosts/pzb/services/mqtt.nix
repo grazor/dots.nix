f46b2e0e33ef5b354148c4c4714923c9ab1612a6
Sergey Poryvaev <porivaevs@gmail.com>
2022-06-04 19:56:31 +0300

    Add caddy

--- hosts/pzb/services/mqtt.nix
+++ hosts/pzb/services/mqtt.nix
@@ -8,7 +8,7 @@ let
     extraOpt = "--pull=always -v /etc/localtime:/etc/localtime:ro --net=host";
 in
 {
-  systemd.services.hass = {
+  systemd.services.mqtt = {
     Service.Description = description;
     Service.Requires = "docker.service";
     Service.After = "network.target";
@@ -16,8 +16,8 @@ in
 
     Service.Restart = "always";
     Service.RestartSec = "3";
-    Service.ExecStart = "${pkgs.docker} run --name=${name} ${configOpt} ${extraOpt} ${image}";
-    Service.ExecStop = "${pkgs.docker} stop -t 2 ${name}";
-    Service.ExecStopPost = "${pkgs.docker} rm -f ${name}";
+    Service.ExecStart = "${pkgs.docker}/bin/docker run --name=${name} ${configOpt} ${extraOpt} ${image}";
+    Service.ExecStop = "${pkgs.docker}/bin/docker stop -t 2 ${name}";
+    Service.ExecStopPost = "${pkgs.docker}/bin/docker rm -f ${name}";
   };
 }
--- hosts/pzb/services/zigbee2mqtt.nix
+++ hosts/pzb/services/zigbee2mqtt.nix
@@ -8,7 +8,7 @@ let
     extraOpt = "--pull=always --device=/dev/ttyACM0 -e TZ=Europe/Moscow -v /etc/localtime:/etc/localtime:ro -v /run/udev:/run/udev:ro --privileged=tru --net=host";
 in
 {
-  systemd.services.hass = {
+  systemd.services.zigbee2mqtt = {
     Service.Description = description;
     Service.Requires = "docker.service";
     Service.After = "network.target";
@@ -16,8 +16,8 @@ in
 
     Service.Restart = "always";
     Service.RestartSec = "3";
-    Service.ExecStart = "${pkgs.docker} run --name=${name} ${configOpt} ${extraOpt} ${image}";
-    Service.ExecStop = "${pkgs.docker} stop -t 2 ${name}";
-    Service.ExecStopPost = "${pkgs.docker} rm -f ${name}";
+    Service.ExecStart = "${pkgs.docker}/bin/docker run --name=${name} ${configOpt} ${extraOpt} ${image}";
+    Service.ExecStop = "${pkgs.docker}/bin/docker stop -t 2 ${name}";
+    Service.ExecStopPost = "${pkgs.docker}/bin/docker rm -f ${name}";
   };
 }
