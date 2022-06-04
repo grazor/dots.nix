09f0e9947cdfc5991a21ab8d99a6c2e14b3f1930
Sergey Porivaev <porivaevs@gmail.com>
2022-06-04 04:16:06 +0300

    Fix docker images

--- hosts/pzb/default.nix
+++ hosts/pzb/default.nix
@@ -1,7 +1,7 @@
 { lib, ... }:
 
 {
-  imports = [ ./hardware.nix ./services.nix ./gui.nix ./users.nix ./services ];
+  imports = [ ./hardware.nix ./gui.nix ./users.nix ./services ];
 
   nix.settings.max-jobs = lib.mkDefault 4;
 }
