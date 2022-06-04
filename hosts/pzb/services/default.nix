bf201af72c98a605d788c869ccbc93ca4cbb9996
Sergey Poryvaev <porivaevs@gmail.com>
2022-06-04 20:35:39 +0300

    Enable influx

--- hosts/pzb/services/default.nix
+++ hosts/pzb/services/default.nix
@@ -1,7 +1,7 @@
 { ... }:
 
 {
-  imports = [ ./hass.nix ./mqtt.nix ./zigbee2mqtt.nix ./caddy.nix ];
+  imports = [ ./hass.nix ./mqtt.nix ./zigbee2mqtt.nix ./caddy.nix ./influx.nix ];
   #imports = [ ./hass.nix ./mqtt.nix ./zigbee2mqtt.nix ./influx.nix ./grafana.nix ];
 }
 
