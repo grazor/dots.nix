473ed1ec0f6369a36000ed976e5bbb68e5ff8365
Sergey Poryvaev <porivaevs@gmail.com>
2022-06-04 19:56:31 +0300

    Add caddy

--- hosts/pzb/services/caddy.nix
+++ hosts/pzb/services/caddy.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 
 let
-    description = "Home Assistant";
-    name = "hass";
-    image = "homeassistant/generic-x86-64-homeassistant";
-    configOpt = "-v /home/cloud/hass/:/config";
+    description = "Caddy Server";
+    name = "caddy";
+    image = "abiosoft/caddy";
+    configOpt = "-v /home/cloud/caddy/Caddyfile:/etc/Caddyfile -v /home/cloud/caddy/certs:/root/.caddy";
     extraOpt = "--pull=always -v /etc/localtime:/etc/localtime:ro --privileged --net=host";
 in
 {
-  systemd.services.hass = {
+  systemd.services.caddy = {
     description = description;
 
     wantedBy = [ "multi-user.target" ];
