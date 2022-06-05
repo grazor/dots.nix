69dd52c8e217a808e409bbb89ec4997a5855235f
Sergey Poryvaev <porivaevs@gmail.com>
2022-06-05 19:14:52 +0300

    Network

--- hosts/pzb/default.nix
+++ hosts/pzb/default.nix
@@ -1,7 +1,7 @@
 { lib, ... }:
 
 {
-  imports = [ ./hardware.nix ./gui.nix ./users.nix ./services ];
+  imports = [ ./hardware.nix ./gui.nix ./users.nix ./services ./network.nix ];
 
   nix.settings.max-jobs = lib.mkDefault 4;
 }
