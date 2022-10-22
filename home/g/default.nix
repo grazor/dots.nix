c4ea2edb8d54e01d31075cf5d6c2ed6229cac8b6
Sergey Poryvaev <porivaevs@gmail.com>
2022-10-23 00:06:32 +0300

    Cfg

--- home/g/default.nix
+++ home/g/default.nix
@@ -3,6 +3,10 @@
 {
   imports = [ ./sway.nix ./i3.nix ./termite.nix ./nvim.nix ./bin.nix ./zsh.nix ./lefthook.nix ];
 
+  # https://github.com/NixOS/nixpkgs/issues/196651
+  # todo: remove workaround
+  manual.manpages.enable = false;
+
   home.stateVersion = "22.11";
   #home.packages = with pkgs; [ ];
 }
