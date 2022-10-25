e0ffd6ac9d3db80b2422956bbfd936957af93851
Sergey Poryvaev <porivaevs@gmail.com>
2022-10-25 18:47:07 +0300

    Add zfs

--- hosts/srv/default.nix
+++ hosts/srv/default.nix
@@ -1,7 +1,7 @@
 { lib, ... }:
 
 {
-  imports = [ ./hardware.nix ./users.nix ./services ./network.nix ./smb.nix ];
+  imports = [ ./hardware.nix ./users.nix ./services ./network.nix ./smb.nix ./zfs.nix ];
 
   nix.settings.max-jobs = lib.mkDefault 4;
 }
