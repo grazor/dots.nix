b6eb55cbf3bff568f33e54d737470bb187d7a4ab
Sergey Poryvaev <porivaevs@gmail.com>
2023-02-22 16:13:31 +0300

    Hyprland

---
+++ home/g/hyprland.nix
@@ -0,0 +1,19 @@
+{ pkgs, lib, config, ... }:
+
+with lib;
+
+{
+  home.packages = with pkgs; [
+	wofi
+	eww # bar
+	swaybg
+	wlsunset
+    mako # notification daemon
+    wl-clipboard # clipboard tool
+    clipman # clipboard manager
+    slurp # screen area selection
+    grim # image grabber
+  ];
+
+  xdg.configFile."hypr/hyprland.conf".source = ./hyprland.conf
+}
