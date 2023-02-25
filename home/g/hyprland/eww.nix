eef4f4da6faba0e93017be1de389bc9ac93351a7
Sergey Poryvaev <porivaevs@gmail.com>
2023-02-25 17:44:51 +0300

    Disable hyprland

--- home/g/hyprland/eww.nix
+++ home/g/hyprland/eww.nix
@@ -4,40 +4,40 @@ with lib;
 
 {
   home.packages = with pkgs; [
-	eww-wayland # bar
-	pamixer
-	brightnessctl
-	(nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    eww-wayland # bar
+    pamixer
+    brightnessctl
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
 
-	# configuration
-	xdg.configFile."eww/eww.scss".source = ./eww.scss;
-	xdg.configFile."eww/eww.yuck".source = ./eww.yuck;
+  # configuration
+  xdg.configFile."eww/eww.scss".source = ./eww.scss;
+  xdg.configFile."eww/eww.yuck".source = ./eww.yuck;
 
-	# scripts
-	xdg.configFile."eww/scripts/battery.sh" = {
-		source = ./scripts/battery.sh;
-		executable = true;
-	};
+  # scripts
+  xdg.configFile."eww/scripts/battery.sh" = {
+    source = ./scripts/battery.sh;
+    executable = true;
+  };
 
-	xdg.configFile."eww/scripts/wifi.sh" = {
-		source = ./scripts/wifi.sh;
-		executable = true;
-	};
+  xdg.configFile."eww/scripts/wifi.sh" = {
+    source = ./scripts/wifi.sh;
+    executable = true;
+  };
 
-	xdg.configFile."eww/scripts/brightness.sh" = {
-		source = ./scripts/brightness.sh;
-		executable = true;
-	};
+  xdg.configFile."eww/scripts/brightness.sh" = {
+    source = ./scripts/brightness.sh;
+    executable = true;
+  };
 
-	xdg.configFile."eww/scripts/workspaces.sh" = {
-		source = ./scripts/workspaces.sh;
-		executable = true;
-	};
+  xdg.configFile."eww/scripts/workspaces.sh" = {
+    source = ./scripts/workspaces.sh;
+    executable = true;
+  };
 
-	xdg.configFile."eww/scripts/workspaces.lua" = {
-		source = ./scripts/workspaces.lua;
-		executable = true;
-	};
+  xdg.configFile."eww/scripts/workspaces.lua" = {
+    source = ./scripts/workspaces.lua;
+    executable = true;
+  };
 }
 
