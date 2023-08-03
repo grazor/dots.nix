bcf87301fa04382a72e209610ba693d8c8b26cee
Sergey Poryvaev <porivaevs@gmail.com>
2023-08-04 00:53:47 +0300

    Eww conf

--- home/g/hyprland/eww.nix
+++ home/g/hyprland/eww.nix
@@ -15,29 +15,59 @@ with lib;
   xdg.configFile."eww/eww.yuck".source = ./eww.yuck;
 
   # scripts
-  xdg.configFile."eww/scripts/battery.sh" = {
-    source = ./scripts/battery.sh;
+  xdg.configFile."eww/scripts/battery" = {
+    source = ./scripts/battery;
     executable = true;
   };
 
-  xdg.configFile."eww/scripts/wifi.sh" = {
-    source = ./scripts/wifi.sh;
+  xdg.configFile."eww/scripts/mem-ad" = {
+    source = ./scripts/mem-ad;
     executable = true;
   };
 
-  xdg.configFile."eww/scripts/brightness.sh" = {
-    source = ./scripts/brightness.sh;
+  xdg.configFile."eww/scripts/memory" = {
+    source = ./scripts/memory;
     executable = true;
   };
 
-  xdg.configFile."eww/scripts/workspaces.sh" = {
-    source = ./scripts/workspaces.sh;
+  xdg.configFile."eww/scripts/music_info" = {
+    source = ./scripts/music_info;
     executable = true;
   };
 
-  xdg.configFile."eww/scripts/workspaces.lua" = {
-    source = ./scripts/workspaces.lua;
+  xdg.configFile."eww/scripts/pop" = {
+    source = ./scripts/pop;
     executable = true;
+  };
+
+  xdg.configFile."eww/scripts/wifi" = {
+    source = ./scripts/wifi;
+    executable = true;
+  };
+
+  xdg.configFile."eww/scripts/workspace" = {
+    source = ./scripts/workspace;
+    executable = true;
+  };
+
+  xdg.configFile."eww/images/mic.png" = {
+    source = ./images/mic.png;
+    executable = false;
+  };
+
+  xdg.configFile."eww/images/music.png" = {
+    source = ./images/music.png;
+    executable = false;
+  };
+
+  xdg.configFile."eww/images/profile.png" = {
+    source = ./images/profile.png;
+    executable = false;
+  };
+
+  xdg.configFile."eww/images/speaker.png" = {
+    source = ./images/speaker.png;
+    executable = false;
   };
 }
 
