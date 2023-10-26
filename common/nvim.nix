a6b8d58f409aac513f36388c3e355e614bd097bd
Sergey Poryvaev <porivaevs@gmail.com>
2023-10-27 01:44:36 +0300

    Disable fhs

--- common/nvim.nix
+++ common/nvim.nix
@@ -4,8 +4,8 @@
   environment.variables = { EDITOR = "nvim"; };
 
   environment.systemPackages = with pkgs; [
-    #neovim
-    neovim-nightly
+    neovim
+	# neovim-nightly
     (neovim.override {
       vimAlias = true;
       withNodeJs = true;
