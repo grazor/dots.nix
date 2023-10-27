768ae2f327787d98b0de7a1123e4b5ed468e532f
Sergey Poryvaev <porivaevs@gmail.com>
2023-10-27 14:48:17 +0300

    Refactor shells

--- shells/flake.go.nix
+++ shells/flake.go.nix
@@ -1,28 +1,14 @@
-{
-  description = "wasm-pack setup";
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
 
-  inputs = {
-    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
-  };
+mkShell {
+	hardeningDisable = [ "fortify" ];
 
-  outputs = { nixpkgs, ... }:
-    let system = "x86_64-linux";
-    in {
-      devShell.${system} = let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in (({ pkgs, ... }:
-        pkgs.mkShell {
-		hardeningDisable = [ "fortify" ];
+	buildInputs = [ gcc go_1_20 gopls golangci-lint delve vault universal-ctags ];
+	propagatedBuildInputs = [ stdenv.cc.cc.lib ];
 
-		buildInputs = with pkgs; [ gcc go_1_20 gopls golangci-lint delve vault universal-ctags ];
-		propagatedBuildInputs = with pkgs; [ stdenv.cc.cc.lib ];
-
-		shellHook = ''
-			export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib/
-			export PATH=$GOPATH/bin:$PATH
-		'';
-        }) { pkgs = pkgs; });
-    };
+	shellHook = ''
+		export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib/
+		export PATH=$GOPATH/bin:$PATH
+	'';
 }
