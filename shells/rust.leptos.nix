d08af49938263a00c1b55f9b4f246c5405380fe2
Sergey Poryvaev <porivaevs@gmail.com>
2024-03-18 15:55:04 +0300

    Add rust shell

---
+++ shells/rust.leptos.nix
@@ -0,0 +1,37 @@
+{ pkgs }:
+
+with pkgs;
+
+mkShell {
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [
+    openssl
+    cacert
+
+    rust-analyzer
+    rustfmt
+
+    (rust-bin.selectLatestNightlyWith (toolchain:
+      toolchain.default.override {
+        extensions = [ "rust-src" "rust-analyzer" ];
+        targets = [ "wasm32-unknown-unknown" ];
+      }))
+
+    cargo
+    cargo-watch
+    cargo-leptos
+    cargo-make
+
+    trunk
+
+    nodejs
+
+    wasm-pack
+  ] ++ pkgs.lib.optionals pkg.stdenv.isDarwin
+    [ darwin.apple_sdk.frameworks.SystemConfiguration ];
+
+  shellHook = ''
+    PATH=$PATH:$HOME/.cargo/bin
+  '';
+}
