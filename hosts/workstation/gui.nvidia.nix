c7788f3a21141665fdf59c1109278a63e49f1413
Sergey Poryvaev <porivaevs@gmail.com>
2024-03-26 16:12:20 +0300

    Update flake

--- hosts/workstation/gui.nvidia.nix
+++ hosts/workstation/gui.nvidia.nix
@@ -10,15 +10,8 @@ let
   # create patch functions for the specified revision
   nvidia-patch = pkgs.nvidia-patch rev hash;
 
-  nvidiaPackage = config.boot.kernelPackages.nvidiaPackages.production;
-  # nvidiaPackage = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-  #   version = "550.54.14";
-  #   sha256_64bit = "sha256-jEl/8c/HwxD7h1FJvDD6pP0m0iN7LLps0uiweAFXz+M=";
-  #   sha256_aarch64 = "sha256-sProBhYziFwk9rDAR2SbRiSaO7RMrf+/ZYryj4BkLB0=";
-  #   openSha256 = "sha256-F+9MWtpIQTF18F2CftCJxQ6WwpA8BVmRGEq3FhHLuYw=";
-  #   settingsSha256 = "sha256-m2rNASJp0i0Ez2OuqL+JpgEF0Yd8sYVCyrOoo/ln2a4=";
-  #   persistencedSha256 = "sha256-XaPN8jVTjdag9frLPgBtqvO/goB5zxeGzaTU0CdL6C4=";
-  # };
+  nvidiaPackage = config.boot.kernelPackages.nvidiaPackages.latest;
+  # nvidiaPackage = config.boot.kernelPackages.nvidiaPackages.mkDriver {}
 in {
   services.xserver = {
     enable = true;
@@ -40,16 +33,17 @@ in {
 
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = false;
+    powerManagement.enable = true;
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = nvidia-patch.patch-nvenc (nvidia-patch.patch-fbc nvidiaPackage);
+    #package = nvidia-patch.patch-nvenc (nvidia-patch.patch-fbc nvidiaPackage);
+    package = nvidiaPackage;
 
     prime = {
-      # sync.enable = true;
-      offload.enable = true;
-      offload.enableOffloadCmd = true;
+      sync.enable = true;
+      #offload.enable = true;
+      #offload.enableOffloadCmd = true;
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
@@ -61,16 +55,16 @@ in {
     "QT_QPA_PLATFORMTHEME" = "qt5ct";
     "QT_WAYLAND_DISABLE_WINDOWDECORATION" = "1";
 
-    # "LIBVA_DRIVER_NAME" = "nvidia";
-    # "XDG_SESSION_TYPE" = "wayland";
-    # "GBM_BACKEND" = "nvidia-drm";
-    # "WLR_NO_HARDWARE_CURSORS" = "1";
+    "LIBVA_DRIVER_NAME" = "nvidia";
+    "XDG_SESSION_TYPE" = "wayland";
+    "GBM_BACKEND" = "nvidia-drm";
+    "WLR_NO_HARDWARE_CURSORS" = "1";
     "NVD_BACKEND" = "direct";
 
-    #"__NV_PRIME_RENDER_OFFLOAD" = "1";
-    #"__NV_PRIME_RENDER_OFFLOAD_PROVIDER" = "NVIDIA-G0";
-    #"__GLX_VENDOR_LIBRARY_NAME" = "nvidia";
-    # "__VK_LAYER_NV_optimus" = "NVIDIA_only";
+    "__NV_PRIME_RENDER_OFFLOAD" = "1";
+    "__NV_PRIME_RENDER_OFFLOAD_PROVIDER" = "NVIDIA-G0";
+    "__GLX_VENDOR_LIBRARY_NAME" = "nvidia";
+    "__VK_LAYER_NV_optimus" = "NVIDIA_only";
   };
 
   # env = LIBVA_DRIVER_NAME,nvidia
