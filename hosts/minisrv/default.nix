b7e81abba533b0c809dfaa6a9e4166dbaaf2fca8
Sergey Poryvaev <porivaevs@gmail.com>
2024-08-12 18:50:50 +0300

    Disable resolved

--- hosts/minisrv/default.nix
+++ hosts/minisrv/default.nix
@@ -1,9 +1,13 @@
 { lib, ... }:
 
 {
-  imports = [ ./hardware.nix ./wireless.nix ./services ];
+  imports = [
+    ./hardware.nix
+    ./wireless.nix
+    ./services
+  ];
 
   nix.settings.max-jobs = lib.mkDefault 4;
   networking.firewall.enable = false;
+  services.resolved.enable = false;
 }
-
