455cd7778694c8af06e96ea7ea57a42bfc9c3ad5
Sergey Poryvaev <porivaevs@gmail.com>
2024-08-12 18:34:09 +0300

    Add pihole

---
+++ hosts/minisrv/services/pihole.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }:
+
+let
+  description = "Pi-hole";
+  name = "pihole";
+  image = "pihole/pihole";
+  configOpt = "-v /home/cloud/pihole/config:/etc/pihole -v /home/cloud/pihole/dnsmasq.d:/etcdnsmasq.d";
+  extraOpt = "--user 1000 --pull=always -v /etc/localtime:/etc/localtime:ro -p 53:53/tcp -p 53:53/udp -p 8400:80/tcp";
+in
+{
+  systemd.services.grafana = {
+    description = description;
+
+    wantedBy = [ "multi-user.target" ];
+    requires = [ "docker.service" ];
+    after = [ "network.target" ];
+
+    serviceConfig = {
+      Restart = "always";
+      RestartSec = "3";
+      ExecStart = "${pkgs.docker}/bin/docker run --name=${name} ${configOpt} ${extraOpt} ${image}";
+      ExecStop = "${pkgs.docker}/bin/docker stop -t 2 ${name}";
+      ExecStopPost = "${pkgs.docker}/bin/docker rm -f ${name}";
+    };
+  };
+}
