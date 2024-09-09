e535cf7818df3fb62e483b619a255e4974d4b17c
Sergey Poryvaev <porivaevs@gmail.com>
2024-09-10 00:42:39 +0300

    Screen sharing support

--- hosts/workstation/hyprland.nix
+++ hosts/workstation/hyprland.nix
@@ -1,8 +1,12 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   programs.hyprland = {
     enable = true;
     # package = inputs.hyprland.packages.${pkgs.system}.hyprland;
   };
 
-  environment.systemPackages = with pkgs; [ xdg-desktop-portal-hyprland ];
+  environment.systemPackages = with pkgs; [
+    xdg-desktop-portal-hyprland
+    xwaylandvideobridge
+  ];
 }
