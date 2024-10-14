2a14eb9c80cbd286649ee282fc8c8a1f0c4cdee0
Sergey Poryvaev <porivaevs@gmail.com>
2024-10-14 16:45:49 +0300

    Add ts, update flake

---
+++ hosts/minisrv/services/teamspeak.nix
@@ -0,0 +1,14 @@
+{ config, lib, ... }:
+
+{
+  services.teamspeak = {
+    enable = true;
+    openFirewall = true;
+    querySshPort = 10022;
+    queryPort = 10011;
+    queryHttpPort = 10080;
+    fileTransferPort = 30033;
+    defaultVoicePort = 9987;
+    dataDir = "/home/cloud/teamspeak";
+  };
+}
