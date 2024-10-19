7e792266720abfa599905354eb1ca8520124fbdc
Sergey Poryvaev <porivaevs@gmail.com>
2024-10-19 21:10:33 +0300

    Add vaultwarden

---
+++ hosts/minisrv/services/vaultwarden.nix
@@ -0,0 +1,21 @@
+{ config, lib, ... }:
+
+{
+  services.vaultwarden = {
+    enable = true;
+    config = {
+      dbBackend = "postgresql";
+      config = {
+        domain = "https://vw.porivaev.ru";
+        signupsAllowed = false;
+        rocketPort = 3011;
+        websocketEnabled = true;
+      };
+    };
+  };
+
+  users = {
+    users.vaultwarden.name = "cloud";
+    groups.vaultwarden.name = "users";
+  };
+}
