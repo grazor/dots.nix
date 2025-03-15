cecd971634454ba79e5aebf6e960052652cf49da
Sergey Porivaev <porivaevs@gmail.com>
2025-03-16 02:36:02 +0300

    Refactor structure

--- flake.nix
+++ flake.nix
@@ -5,28 +5,40 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:rycee/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs @ {
     self,
     nix-darwin,
+    home-manager,
     nixpkgs,
   }: let
     #system.configurationRevision = self.rev or self.dirtyRev or null;
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-darwin"
-      ] (system: function nixpkgs.legacyPackages.${system});
+    linuxSystem = "x86_64-linux";
+    darwinSystem = "aarch64-darwin";
+    allSystems = [linuxSystem darwinSystem];
   in {
-    packages = forAllSystems (pkgs: {
-      default = pkgs.callPackage ./package.nix {};
-    });
+    nixosConfigurations."minisrv" = let
+      system = linuxSystem;
+    in
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          (import ./hosts/darwin/minisrv)
+        ];
+      };
 
-    darwinConfigurations."MSK-GRVQ3CV9RQ" = nix-darwin.lib.darwinSystem {
-      modules = [
-        (import ./hosts/darwin/default.nix)
-      ];
-    };
+    darwinConfigurations."MSK-GRVQ3CV9RQ" = let
+      system = darwinSystem;
+      pkgs = nixpkgs.legacyPackages.${darwinSystem};
+    in
+      nix-darwin.lib.darwinSystem {
+        inherit system;
+        modules = import ./hosts/darwin/modules.nix (inputs // {inherit system pkgs;});
+      };
+
+    shells = import ./shells {inherit allSystems nixpkgs;};
   };
 }
