ba29b8a9f5325d371a4f217894c8e1b18317f10e
Sergey Poryvaev <porivaevs@gmail.com>
2025-04-09 22:39:18 +0300

    Update flakes

--- hosts/dell/system/default.nix
+++ hosts/dell/system/default.nix
@@ -4,13 +4,14 @@ inputs @ {lib, ...}: {
     ./nvidia.nix
 
     (import ../../_common/system/nix.nix inputs)
-    ../../_common/system/tools.nix
     ../../_common/system/devtools.nix
-    ../../_common/system/linux.nix
+    ../../_common/system/devtools.nix
     ../../_common/system/docker.nix
-    ../../_common/system/wireless.nix
+    ../../_common/system/linux.nix
     ../../_common/system/pipewire.nix
+    ../../_common/system/tools.nix
     ../../_common/system/udev.nix
+    ../../_common/system/wireless.nix
   ];
 
   services.logrotate.checkConfig = false;
