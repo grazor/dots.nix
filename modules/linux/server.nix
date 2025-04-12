d85ebbcad87c648d4369e2517b3c041c645a7128
Sergey Poryvaev <porivaevs@gmail.com>
2025-04-12 17:14:18 +0300

    Server mode

---
+++ modules/linux/server.nix
@@ -0,0 +1,15 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.grazor.linux;
+in {
+  options.grazor.linux.asServer = lib.mkEnableOption "act as server";
+  config = lib.mkIf cfg.asServer {
+    services.logind.lidSwitch = "ignore";
+    boot.kernelParams = ["consoleblank=120"];
+    grazor.sshServer = lib.mkForce true;
+    grazor.withAuthorizedKeys = lib.mkForce true;
+  };
+}
