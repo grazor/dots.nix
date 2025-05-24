8df38043fd25921b307640ac6d265bb9f7248f82
Sergey Porivaev <porivaevs@gmail.com>
2025-05-25 01:30:51 +0300

    Add desktop

---
+++ hosts/desktop/config.nix
@@ -0,0 +1,42 @@
+{pkgs, ...}: {
+  grazor = {
+    user = rec {
+      uid = 1000;
+      name = "g";
+      gid = 100;
+      group = "users";
+      home = "/home/${name}";
+      shell = pkgs.fish;
+
+      config = {
+        withGit = true;
+        withTmux = true;
+        shellInitTmux = true;
+        withFish = true;
+        withScripts = true;
+      };
+    };
+
+    linux = {
+      withCommon = true;
+      withWireless = true;
+      withDocker = true;
+      withUserDefaults = true;
+      withUdev = true;
+      withPipewire = true;
+      asServer = false;
+
+      withNvidia = true;
+      withIntel = true;
+      withHyprland = true;
+      withGuiApps = true;
+    };
+
+    withTools = true;
+    withDevtools = true;
+    withNvf = true;
+    withFonts = true;
+    withAuthorizedKeys = true;
+    sshServer = true;
+  };
+}
