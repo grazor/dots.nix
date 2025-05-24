e31d85d6ea3cc9c8cd7036b9b91f5a681fc92d77
Sergey Poryvaev <porivaevs@gmail.com>
2025-05-25 01:36:16 +0300

    Finx config

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -4,13 +4,12 @@
     ./config.nix
   ];
 
-  networking.hostName = "dell";
-  system.stateVersion = "24.11";
+  networking.hostName = "desktop";
+  system.stateVersion = "25.05";
   nix.settings.max-jobs = lib.mkDefault 8;
 
   environment.enableAllTerminfo = true;
   services.logrotate.checkConfig = false;
 
   networking.firewall.enable = false;
-  services.resolved.enable = lib.mkForce false;
 }
--- hosts/desktop/hardware.nix
+++ hosts/desktop/hardware.nix
@@ -11,24 +11,22 @@
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   boot = {
-    initrd.availableKernelModules = ["xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"];
+    initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
     initrd.kernelModules = [];
     kernelModules = ["kvm-intel"];
     extraModulePackages = [];
   };
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/65b0d516-76fe-4a82-bd43-ab937680d82b";
-    fsType = "ext4";
-  };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/1c2eeb31-ea2c-42ae-9c39-645a52310687";
+      fsType = "ext4";
+    };
 
-  boot.initrd.luks.devices."luks-8da9eb70-9a52-4935-82ca-69d39e465873".device = "/dev/disk/by-uuid/8da9eb70-9a52-4935-82ca-69d39e465873";
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/A2D3-9A85";
-    fsType = "vfat";
-    options = ["fmask=0077" "dmask=0077"];
-  };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/C87A-FB49";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
 
   swapDevices = [];
 
