c75fc2384cd1c51372bbdf66a58c2dee24452d05
Sergey Poryvaev <porivaevs@gmail.com>
2025-05-26 01:10:48 +0300

    Improve performance

--- hosts/desktop/hardware.nix
+++ hosts/desktop/hardware.nix
@@ -11,25 +11,25 @@
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   boot = {
-    initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+    initrd.availableKernelModules = ["xhci_pci" "ehci_pci" "nvme" "usbhid" "usb_storage" "sd_mod"];
     initrd.kernelModules = [];
     kernelModules = ["kvm-intel"];
     extraModulePackages = [];
   };
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/1c2eeb31-ea2c-42ae-9c39-645a52310687";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/1c2eeb31-ea2c-42ae-9c39-645a52310687";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/C87A-FB49";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/C87A-FB49";
+    fsType = "vfat";
+    options = ["fmask=0077" "dmask=0077"];
+  };
 
   swapDevices = [];
 
   hardware.bluetooth.enable = true;
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
 }
