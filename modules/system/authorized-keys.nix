3984025076cb32e6d28b3794def24907d2c42ff9
Sergey Poryvaev <porivaevs@gmail.com>
2025-06-20 23:28:19 +0300

    Disable md

--- modules/system/authorized-keys.nix
+++ modules/system/authorized-keys.nix
@@ -9,7 +9,7 @@ in {
   options.grazor.withAuthorizedKeys = lib.mkEnableOption "with authorized keys setup";
   config = lib.mkIf cfg.withAuthorizedKeys {
     users.users.${username}.openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIALBrsfuH5DBf5X3r5EoEUoJLr1fbAOIYFBY9d2BNEo1 deskw"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOn5dz1yRCugoCCeQbLIL8GJ36e7vlv48bPQ6dem0myc deskw"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJJLUzThrMDq7aDnSB3gQzpd8RVI5jwghcxh/11dgsYW deskn"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFDW3nxkH6KTgkDKyI9tgc9yhSPlruzSiIxXBnS4A6JY work"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEs9zEKgXOgoUr9thW7WsoBnPU3cVTSYsdfdMknmf7PG emerg"
