f5a10cc23f948b0a73dd82b9dd46c0df11bd8047
Sergey Poyvaev <porivaevs@gmail.com>
2025-07-22 22:09:42 +0300

    Add rpi config

---
+++ hosts/hl-rpi-mqtt/hardware.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  lib,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "usbhid" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+      fsType = "ext4";
+    };
+
+  fileSystems."/mnt" =
+    { device = "/dev/disk/by-uuid/2178-694E";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+
+  hardware.bluetooth.enable = true;
+}
