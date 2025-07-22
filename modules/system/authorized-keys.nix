587678df1b61e468fcbc37dc780556a2789eb577
Sergey Poryvaev <porivaevs@gmail.com>
2025-07-23 02:41:22 +0300

    Add key

--- modules/system/authorized-keys.nix
+++ modules/system/authorized-keys.nix
@@ -9,12 +9,12 @@ in {
   options.grazor.withAuthorizedKeys = lib.mkEnableOption "with authorized keys setup";
   config = lib.mkIf cfg.withAuthorizedKeys {
     users.users.${username}.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEs9zEKgXOgoUr9thW7WsoBnPU3cVTSYsdfdMknmf7PG emerg"
+
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOn5dz1yRCugoCCeQbLIL8GJ36e7vlv48bPQ6dem0myc deskw"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJJLUzThrMDq7aDnSB3gQzpd8RVI5jwghcxh/11dgsYW deskn"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFDW3nxkH6KTgkDKyI9tgc9yhSPlruzSiIxXBnS4A6JY work"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEs9zEKgXOgoUr9thW7WsoBnPU3cVTSYsdfdMknmf7PG emerg"
-
-      #"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINEcA3dCkiqDQtsoP0MicTylt6rQCntGf6XeWif0TecA cloud"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINEcA3dCkiqDQtsoP0MicTylt6rQCntGf6XeWif0TecA cloud"
     ];
   };
 }
