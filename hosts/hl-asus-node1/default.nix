6c7bbb7fe3b039e206bc795c6bf9a422f341661e
Sergey Poryvaev <porivaevs@gmail.com>
2025-07-24 02:23:27 +0300

    Asus config

--- hosts/hl-asus-node1/default.nix
+++ hosts/hl-asus-node1/default.nix
@@ -1,16 +1,25 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     ./hardware.nix
     ./config.nix
   ];
 
   networking.hostName = "hl-asus-node1";
+  networking.useDHCP = true;
+  networking.firewall.enable = false;
+  networking.wireless.iwd.enable = false;
+  networking.networkmanager.wifi.backend = "wpa_supplicant";
+  environment.systemPackages = [pkgs.wirelesstools];
+
   system.stateVersion = "25.05";
   nix.settings.max-jobs = lib.mkDefault 4;
 
   environment.enableAllTerminfo = true;
   services.logrotate.checkConfig = false;
 
-  networking.firewall.enable = false;
   services.resolved.enable = lib.mkForce false;
 }
