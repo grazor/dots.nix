c2004cf7f34b5a64d3f2f482bdbc4f5ea80d1e56
Sergey Porivaev <porivaevs@gmail.com>
2025-08-02 16:28:17 +0300

    Python3 flake

--- shells/python3.nix
+++ shells/python3.nix
@@ -1,45 +1,43 @@
-{pkgs, ...}:
-pkgs.mkShell {
-  name = "python3";
-  venvDir = "./.venv.py3";
+{pkgs, ...}: let
+  pythonPackages = pkgs.python312Packages;
+in
+  pkgs.mkShell {
+    name = "python3";
+    venvDir = "./.venv.py3";
 
-  buildInputs = with pkgs; [
-    pythonPackages.python
-    pythonPackages.venvShellHook
-    pythonPackages.six
-    pythonPackages.certifi
-    pythonPackages.requests
-    pythonPackages.pip
-    pythonPackages.python-lsp-server
+    buildInputs = with pkgs; [
+      pythonPackages.python
+      pythonPackages.venvShellHook
+      pythonPackages.six
+      pythonPackages.certifi
+      pythonPackages.requests
+      pythonPackages.pip
+      pythonPackages.python-lsp-server
 
-    ruff
-    taglib
-    openssl
-    git
-    libxml2
-    libxslt
-    libzip
-    zlib
-  ];
+      ruff
+      taglib
+      openssl
+      git
+      libxml2
+      libxslt
+      libzip
+      zlib
+    ];
 
-  propagatedBuildInputs = with pkgs; [
-    pythonPackages.setuptools
-    pythonPackages.six
-    stdenv.cc.cc.lib
-  ];
+    propagatedBuildInputs = with pkgs; [stdenv.cc.cc.lib];
 
-  postVenvCreation = ''
-    unset SOURCE_DATE_EPOCH
-    pip install pdbpp poetry jupyter
-  '';
+    postVenvCreation = ''
+      unset SOURCE_DATE_EPOCH
+      pip install pdbpp poetry jupyter
+    '';
 
-  postShellHook = ''
-    unset SOURCE_DATE_EPOCH
-    export LD_LIBRARY_PATH=${
-      pkgs.lib.makeLibraryPath [
-        pkgs.stdenv.cc.cc
-        pkgs.libGL
-      ]
-    }
-  '';
-}
+    postShellHook = ''
+      unset SOURCE_DATE_EPOCH
+      export LD_LIBRARY_PATH=${
+        pkgs.lib.makeLibraryPath [
+          pkgs.stdenv.cc.cc
+          pkgs.libGL
+        ]
+      }
+    '';
+  }
