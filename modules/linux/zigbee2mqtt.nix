51472eb3b5c7994ce4ce9c63e6635b25c1083766
Sergey Poryvaev <porivaevs@gmail.com>
2025-08-10 01:23:37 +0300

    Enable z2m

--- modules/linux/zigbee2mqtt.nix
+++ modules/linux/zigbee2mqtt.nix
@@ -6,13 +6,12 @@
 }: let
   home = config.grazor.user.home;
   cfg = config.grazor.linux;
-  opt = "zigbee2mqtt"
+  opt = "zigbee2mqtt";
 in {
   options.grazor.linux.${opt}.enable = lib.mkEnableOption "with z2m";
   config = lib.mkIf cfg.${opt}.enable {
     services.zigbee2mqtt = {
       enable = true;
-      dataDir = home + "/" + "zigbee2mqtt";
       settings = {
         permit_join = false;
         frontend.enabled = true;
