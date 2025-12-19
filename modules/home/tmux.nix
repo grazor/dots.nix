0e4948ef8a049d548b9e79f0ac54f5dd47310518
Sergey Porivaev <porivaevs@gmail.com>
2025-12-19 14:27:32 +0300

    Fix tmux powerline config

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -11,36 +11,38 @@ in {
   options.grazor.user.config.shellInitTmux = lib.mkEnableOption "run tmux on init";
 
   config = lib.mkIf cfg.withTmux {
-    home-manager.users.${username}.programs.tmux = {
-      enable = true;
-      keyMode = "vi";
-      baseIndex = 1;
-      clock24 = true;
-      mouse = true;
+    home-manager.users.${username} = {
+      programs.tmux = {
+        enable = true;
+        keyMode = "vi";
+        baseIndex = 1;
+        clock24 = true;
+        mouse = true;
 
-      plugins = with pkgs; [
-        tmuxPlugins.tmux-powerline
-        tmuxPlugins.yank
-        tmuxPlugins.tmux-floax
-      ];
+        plugins = with pkgs; [
+          tmuxPlugins.tmux-powerline
+          tmuxPlugins.yank
+          tmuxPlugins.tmux-floax
+        ];
 
-      home.sessionVariables = {
-        TMUX_POWERLINE_LEFT_STATUS_SEGMENTS = ["tmux_session_info 148 234"];
-        TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS = ["pwd 89 211"];
+        extraConfig = ''
+          set-option -g renumber-windows on
+
+          bind-key -n C-l if-shell "$is_vim" "send-keys C-l"  "send-keys C-l"
+
+          bind _ split-window -h -c "#{pane_current_path}"
+          bind - split-window -v -c "#{pane_current_path}"
+          bind h select-pane -L
+          bind j select-pane -D
+          bind k select-pane -U
+          bind l select-pane -R
+        '';
       };
 
-      extraConfig = ''
-        set-option -g renumber-windows on
-
-        bind-key -n C-l if-shell "$is_vim" "send-keys C-l"  "send-keys C-l"
-
-        bind _ split-window -h -c "#{pane_current_path}"
-        bind - split-window -v -c "#{pane_current_path}"
-        bind h select-pane -L
-        bind j select-pane -D
-        bind k select-pane -U
-        bind l select-pane -R
-      '';
+      home.sessionVariables = {
+        "TMUX_POWERLINE_LEFT_STATUS_SEGMENTS" = "tmux_session_info 148 234";
+        "TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS" = "pwd 89 211";
+      };
     };
   };
 }
