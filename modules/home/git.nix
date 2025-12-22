76304243c25eea83234f8c4583b9da1b09ba53f4
Sergey Porivaev <porivaevs@gmail.com>
2025-12-23 01:50:42 +0300

    Disable git hints

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -8,35 +8,41 @@
 in {
   options.grazor.user.config.withGit = lib.mkEnableOption "with git config";
   config = lib.mkIf cfg.withGit {
-    home-manager.users.${username}.programs.git = {
-      enable = true;
-      lfs.enable = true;
+    home-manager.users.${username} = {
+      programs.git = {
+        enable = true;
+        lfs.enable = true;
 
-      settings = {
-        user.name = "Sergey Poryvaev";
-        user.email = "porivaevs@gmail.com";
-        alias = {
-          ci = "commit";
-          co = "checkout";
-          s = "status";
+        settings = {
+          user.name = "Sergey Poryvaev";
+          user.email = "porivaevs@gmail.com";
+          alias = {
+            ci = "commit";
+            co = "checkout";
+            s = "status";
+          };
         };
+
+        ignores = [
+          ".direnv"
+          ".envrc"
+          ".lefthook"
+          ".venv"
+          "Pipfile"
+          "Pipfile.lock"
+          "__debug_bin"
+          "_build"
+          "debug.test"
+          "lefthook.yaml"
+          "lefthook.yml"
+          "ptest"
+          "tags"
+        ];
       };
 
-      ignores = [
-        ".direnv"
-        ".envrc"
-        ".lefthook"
-        ".venv"
-        "Pipfile"
-        "Pipfile.lock"
-        "__debug_bin"
-        "_build"
-        "debug.test"
-        "lefthook.yaml"
-        "lefthook.yml"
-        "ptest"
-        "tags"
-      ];
+      home.sessionVariables = {
+        "GIT_ADVICE" = "0";
+      };
     };
   };
 }
