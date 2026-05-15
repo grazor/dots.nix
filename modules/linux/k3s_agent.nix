5d5e038fa3e83b3bdee3e58957ec51970d1f7a74
smporyvaev <smporyvaev@avito.ru>
2026-05-15 14:49:43 +0300

    Update k3s symlinks

--- modules/linux/k3s_agent.nix
+++ modules/linux/k3s_agent.nix
@@ -58,5 +58,30 @@ in {
       tokenFile = userHome + "/.token.k3s";
       serverAddr = cfg.${optServerAddr};
     };
+
+    systemd.tmpfiles.rules = [
+      "L+ /usr/local/bin/iscsiadm - - - - /run/current-system/sw/bin/iscsiadm"
+      "L+ /usr/bin/iscsiadm - - - - /run/current-system/sw/bin/iscsiadm"
+      "L+ /usr/sbin/iscsiadm - - - - /run/current-system/sw/bin/iscsiadm"
+
+      "L+ /usr/local/bin/mount.nfs - - - - /run/current-system/sw/bin/mount.nfs"
+      "L+ /usr/local/bin/mount.nfs4 - - - - /run/current-system/sw/bin/mount.nfs4"
+      "L+ /usr/bin/mount.nfs - - - - /run/current-system/sw/bin/mount.nfs"
+      "L+ /usr/bin/mount.nfs4 - - - - /run/current-system/sw/bin/mount.nfs4"
+
+      # Filesystem tools (mkfs/resize for volume expansion)
+      "L+ /usr/local/bin/mkfs.ext4 - - - - /run/current-system/sw/bin/mkfs.ext4"
+      "L+ /usr/local/bin/mkfs.xfs - - - - /run/current-system/sw/bin/mkfs.xfs"
+      "L+ /usr/bin/mkfs.ext4 - - - - /run/current-system/sw/bin/mkfs.ext4"
+      "L+ /usr/bin/mkfs.xfs - - - - /run/current-system/sw/bin/mkfs.xfs"
+
+      # nsenter itself (some components call the host nsenter)
+      "L+ /usr/local/bin/nsenter - - - - /run/current-system/sw/bin/nsenter"
+      "L+ /usr/bin/nsenter - - - - /run/current-system/sw/bin/nsenter"
+
+      # blkid (device identification)
+      "L+ /usr/local/bin/blkid - - - - /run/current-system/sw/bin/blkid"
+      "L+ /usr/bin/blkid - - - - /run/current-system/sw/bin/blkid"
+    ];
   };
 }
